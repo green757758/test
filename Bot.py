48dbcce26389fd83b5b9dcee003bdabece31c3f1
Hope <wxc4291225>
2023-03-28 15:51:26 +0800

    第一次提交

---
+++ Bot.py
@@ -0,0 +1,22 @@
+import logging
+import telepot
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+def send_message():
+    bot.sendMessage("Chat_id", "This file is about ... please check")
+    bot.sendDocument("Chat_id", document=open("./test1.xlsx", "rb"))
+
+
+def send_phone():
+    bot.sendPhoto("Chat_id", photo=open("test1.jpg", "rb"))
+
+
+if __name__ == '__main__':
+    bot = telepot.Bot("TOKEN")
+    # send_message()
+    send_phone()
